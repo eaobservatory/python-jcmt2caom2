973f6eeb28f988d2322bc30253b6eed7ef9e9a1e
Russell Redman <russell.o.redman@shaw.ca>
2014-05-29 21:22:20 -0700

    rationalize names and usage of logs and shell scripts

--- jcmt2caom2/rawwrap.py
+++ jcmt2caom2/rawwrap.py
@@ -30,16 +30,14 @@ def run():
     rawutdate --debug --start=20100123 --end=20100131
     """
     obsid_regex = re.compile('^\s*((acsis|scuba2|DAS|AOSC)_\d{5}_\d{8}T\d{6})')
+    utdate_str = utdate_string()
     
     ap = argparse.ArgumentParser('jcmtrawwrap')
     ap.add_argument('--log',
-                    default='jcmtrawwrap_' + utdate_string() + '.log',
+                    default='jcmtrawwrap_' + utdate_str + '.log',
                     help='(optional) name of log file')
     ap.add_argument('--logdir',
                     help='(optional) directory to hold log files')
-    ap.add_argument('--sharelog',
-                    action='store_true',
-                    help='share the same log file for all ingestions')
 
     ap.add_argument('--outdir',
                     help='(optional) output directory for working files')
@@ -91,164 +89,150 @@ def run():
     else:
         logpath = os.path.join(logdir, a.log)
 
-    log = logger(logpath, loglevel)
-    log.file('jcmt2caom2version    = ' + jcmt2caom2version)
-    log.file('tools4caom2version   = ' + tools4caom2version)
-    for attr in dir(a):
-        if attr != 'id' and attr[0] != '_':
-            log.console('%-15s= %s' % (attr, getattr(a, attr)))
-    log.console('id = ' + repr(a.id))
-    
-    if a.qsubrequirements:
-        mygridengine = gridengine(log, 
-                                  queue=a.queue,
-                                  options=a.qsubrequirements)
-    else:
-        mygridengine = gridengine(log, queue=a.queue)
-     
-    # idset is the set of recipe instances to ingest
-    idset = set()
-    # obsidset is a set of abspaths to obsid files
-    obsidset = set()
-    for id in a.id:
-        # if id is a directory, add any obsid files it contains to obsidset
-        if os.path.isdir(id):
-            idpath = os.path.abspath(id)
-            for filename in os.listdir(idpath):
-                basename, ext = os.path.splitext(filename)
-                if ext == '.obsid':
-                    obsidset.add(os.path.join(idpath, filename))
-        elif os.path.exists(id):
-            # if id is an obsid file add it to obsidset
-            basename, ext = os.path.splitext(id)
-            if ext == '.obsid':
-                obsidset.add(os.path.abspath(id))
-        elif obsid_regex.search(id):
-            # if id is an observationID string, add it to idset
-            m = obsid_regex.search(id)
-            idset.add(id.group[1])
+    with logger(logpath, loglevel).record() as log:
+        log.file(sys.argv[0])
+        log.file('tools4caom2version   = ' + tools4caom2version)
+        log.file('jcmt2caom2version    = ' + jcmt2caom2version)
+        for attr in dir(a):
+            if attr != 'id' and attr[0] != '_':
+                log.console('%-15s= %s' % (attr, getattr(a, attr)))
+        log.console('id = ' + repr(a.id))
+        
+        if a.qsubrequirements:
+            mygridengine = gridengine(log, 
+                                      queue=a.queue,
+                                      options=a.qsubrequirements)
         else:
-            log.console(id + ' is not a directory, and obsid file, nor an '
-                        'OBSID value',
-                        logging.WARN)
-    
-    log.file('idset = ' + repr(idset))
-    log.file('obsidset = ' + repr(obsidset))
-    
-    retvals = None
-    if a.qsub:
-        rawcmd = 'jcmtrawwrap'
-        rawcmd += ' --outdir=${TMPDIR}'
-        if a.debug:
-            rawcmd += ' --debug'
-        if a.sharelog:
-            rawcmd += ' --sharelog'
-            
-        # submit obsid sets to gridengine
-        # compose the jcmtrawwrap command
-        for obsidfile in sorted(list(obsidset)):
-            cmd = rawcmd
-            obsidbase = os.path.basename(obsidfile)
-            obsidlog =  os.path.join(logdir, obsidbase + '.log')
-            obsidcsh = os.path.join(logdir, 'csh_' + obsidbase + '.csh')
-            
-            if a.sharelog:
-                cmd += ' --sharelog'
-        
-            obsidlogs = os.path.join(logdir, obsidbase + '.logs')
-            if not os.path.isdir(obsidlogs):
-                os.makedirs(obsidlogs)
-            else:
-                # make sure obsidlogs is empty
-                for f in os.listdir(obsidlogs):
-                    os.remove(os.path.join(obsidlogs, f))
-                
-            cmd += ' --log=' + obsidlog
-            cmd += ' --logdir=' + obsidlogs
-            cmd += ' ' + obsidfile
-            
-            log.console('SUBMIT: ' + cmd)
-            if not a.test:
-                mygridengine.submit(cmd, obsidcsh, obsidlog)
-        
-        # If any obsid values were specified in the command line, 
-        # submit them as well
-        if idset:
-            idlist = [rawcmd]
-            idlist.extend(sorted(list(idset), key=int, reverse=True))
-            cmd = ' '.join(idlist)
-
-            obsidcsh = os.path.join(logdir, 'obsid_list.csh')
-            obsidlog = os.path.join(logdir, 'obsid_list.log')
-            
-            log.console('SUBMIT: ' + cmd)
-            if not a.test:
-                mygridengine.submit(cmd, obsidcsh, obsidlog)
-
-    else:
-        # ingest the recipe instances in subprocesses
-        for obsidfile in list(obsidset):
-            with open(obsidfile) as OF:
-                for line in OF:
-                    m = obsid_regex.search(line)
-                    if m:
-                        thisid = m.group(1)
-                        log.console('found ' + thisid,
-                                    logging.DEBUG)
-                        idset.add(thisid)
-
-        rawcmd = 'jcmt2Caom2DA --full'
-        if a.debug:
-            rawcmd += ' --debug'
-        
-        idlist = sorted(list(idset))
-
-        # jcmt2Caom2DA does not pass --log or --logdir to 
-        # jcmt2caom2raw, so it is necessary to be in the logdir
-        os.chmod(logdir)
-        
-        for obsid in idlist:
-            thisrawcmd = rawcmd
-
-            if a.sharelog:
-                thisrawcmd += ' --log=' + logpath
-            else:
-                thislog = os.path.join(logdir,
-                                       'raw_' + obsid + '.log')
-                thisrawcmd += ' --log=' + thislog
-            
-            thisrawcmd += (' --start=' + obsid)
-            thisrawcmd += (' --end=' + obsid)
-            thisrawcmd += ' --mode=raw'
-            thisrawcmd += (' --script=' + os.path.join(sys.path[0],
-                                                       'jcmt2caom2raw'))
-        
-            log.console('PROGRESS: ' + thisrawcmd)
-            
-            if not a.test:
-                try:
-                    output = subprocess.check_output(
-                                                thisrawcmd,
-                                                shell=True,
-                                                stderr=subprocess.STDOUT)
-                except subprocess.CalledProcessError as e:
-                    log.console('FAILED: ' + obsid,
-                                logging.WARN)
-                    log.file('status = ' + str(e.returncode) + 
-                             ' output = \n' + e.output)
-                
-                # clean up
-                for filename in os.listdir(cwd):
-                    filepath = os.path.join(cwd, filename)
+            mygridengine = gridengine(log, queue=a.queue)
+         
+        # idset is the set of recipe instances to ingest
+        idset = set()
+        # obsidset is a set of abspaths to obsid files
+        obsidset = set()
+        for id in a.id:
+            # if id is a directory, add any obsid files it contains to obsidset
+            if os.path.isdir(id):
+                idpath = os.path.abspath(id)
+                for filename in os.listdir(idpath):
                     basename, ext = os.path.splitext(filename)
-                    if ext == '.xml':
-                        log.console('rm ' + filepath, logging.DEBUG)
-                        os.remove(filepath)
+                    if ext == '.obsid':
+                        obsidset.add(os.path.join(idpath, filename))
+            elif os.path.exists(id):
+                # if id is an obsid file add it to obsidset
+                basename, ext = os.path.splitext(id)
+                if ext == '.obsid':
+                    obsidset.add(os.path.abspath(id))
+            elif obsid_regex.search(id):
+                # if id is an observationID string, add it to idset
+                m = obsid_regex.search(id)
+                idset.add(id.group[1])
+            else:
+                log.console(id + ' is not a directory, and obsid file, nor an '
+                            'OBSID value',
+                            logging.WARN)
+        
+        log.file('idset = ' + repr(idset))
+        log.file('obsidset = ' + repr(obsidset))
+        
+        retvals = None
+        if a.qsub:
+            rawcmd = 'jcmtrawwrap'
+            rawcmd += ' --outdir=${TMPDIR}'
+            if a.debug:
+                rawcmd += ' --debug'
+                
+            # submit obsid sets to gridengine
+            # compose the jcmtrawwrap command
+            for obsidfile in sorted(list(obsidset)):
+                cmd = rawcmd
+                obsidbase = '_'.join([
+                                os.path.splitext(
+                                    os.path.basename(obsidfile))[0],
+                                utdate_str])
+                obsidlog = os.path.join(logdir, obsidbase + '.log')
+                obsidcsh = os.path.join(logdir, obsidbase + '.csh')
+                
+                obsidlogs = os.path.join(logdir, obsidbase + '_logs')
+                if not os.path.isdir(obsidlogs):
+                    os.makedirs(obsidlogs)
+                    
+                cmd += ' --log=' + obsidlog
+                cmd += ' --logdir=' + obsidlogs
+                cmd += ' ' + obsidfile
+                
+                log.console('SUBMIT: ' + cmd)
+                if not a.test:
+                    mygridengine.submit(cmd, obsidcsh, obsidlog)
+            
+            # If any obsid values were specified in the command line, 
+            # submit them as well
+            if idset:
+                idlist = [rawcmd]
+                idlist.extend(sorted(list(idset), key=int, reverse=True))
+                cmd = ' '.join(idlist)
 
-                #if not a.sharelog:
-                #    gzipcmd = 'gzip ' + thislog
-                #    log.console(gzipcmd, logging.DEBUG)
-                #    output = subprocess.check_output(
-                #                        gzipcmd,
-                #                        shell=True,
-                #                        stderr=subprocess.STDOUT)
+                obsidcsh = os.path.join(logdir, 'obsid_list.csh')
+                obsidlog = os.path.join(logdir, 'obsid_list.log')
+                
+                log.console('SUBMIT: ' + cmd)
+                if not a.test:
+                    mygridengine.submit(cmd, obsidcsh, obsidlog)
+
+        else:
+            # ingest the recipe instances in subprocesses
+            for obsidfile in list(obsidset):
+                with open(obsidfile) as OF:
+                    for line in OF:
+                        m = obsid_regex.search(line)
+                        if m:
+                            thisid = m.group(1)
+                            log.console('found ' + thisid,
+                                        logging.DEBUG)
+                            idset.add(thisid)
+
+            rawpath = os.path.join(sys.path[0], 'jcmt2caom2raw')
+            rawcmd = 'jcmt2Caom2DA --full'
+            if a.debug:
+                rawcmd += ' --debug'
+            
+            idlist = sorted(list(idset))
+
+            # jcmt2Caom2DA does not pass --log or --logdir to 
+            # jcmt2caom2raw, so it is necessary for logdir to be the
+            # current directory
+            os.chdir(logdir)
+            
+            for obsid in idlist:
+                thisrawcmd = rawcmd
+
+                # jcmt2Caom2DA does not log much and does not share a log with 
+                # jcmt2caom2raw, so append any output to the current log
+                thisrawcmd += (' --log=' + logpath)
+                thisrawcmd += (' --start=' + obsid)
+                thisrawcmd += (' --end=' + obsid)
+                thisrawcmd += ' --mode=raw'
+                thisrawcmd += (' --script=' + rawpath)
+            
+                log.console('PROGRESS: ' + thisrawcmd)
+                
+                if not a.test:
+                    try:
+                        output = subprocess.check_output(
+                                                    thisrawcmd,
+                                                    shell=True,
+                                                    stderr=subprocess.STDOUT)
+                    except subprocess.CalledProcessError as e:
+                        log.console('FAILED: ' + obsid,
+                                    logging.WARN)
+                        log.file('status = ' + str(e.returncode) + 
+                                 ' output = \n' + e.output)
+                    
+                    # clean up
+                    for filename in os.listdir(cwd):
+                        filepath = os.path.join(cwd, filename)
+                        basename, ext = os.path.splitext(filename)
+                        if ext == '.xml':
+                            log.console('rm ' + filepath, logging.DEBUG)
+                            os.remove(filepath)
+
+        log.console('DONE')
