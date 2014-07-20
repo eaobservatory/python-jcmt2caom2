5b3a5cb37de2b1200012a258ddb2b0a7fee1f23b
Russell Redman <russell.o.redman@shaw.ca>
2014-07-20 16:09:36 -0700

    add command to search for healpix recipe instances

---
+++ jcmt2caom2/hpx_files.py
@@ -0,0 +1,378 @@
+#!/usr/bin/env python2.7
+
+import argparse
+from ConfigParser import SafeConfigParser
+from datetime import datetime
+from datetime import timedelta
+import logging
+import os.path
+import re
+import stat
+import string
+import subprocess
+import textwrap
+
+from tools4caom2.logger import logger
+from tools4caom2.database import database
+from tools4caom2.database import connection
+from tools4caom2.gridengine import gridengine
+from tools4caom2.utdate_string import utdate_string
+
+from tools4caom2.__version__ import version as tools4caom2version
+from jcmt2caom2.__version__ import version as jcmt2caom2version
+
+def run():
+    """
+    Construct a set of files containing all valid recipe instances stored in
+    files by the utdate of the earliest (alphabetiaclly smallest) input file
+    in dp_file_input.
+    """
+    userconfig = SafeConfigParser()
+    # The server and cred_db are used to get database credentials at the CADC.
+    # Other sites should supply cadc_id, cadc_key in the section [cadc] of
+    # the userconfig file.
+    if not userconfig.has_section('cadc'):
+        userconfig.add_section('cadc')
+    userconfig.set('cadc', 'server', 'SYBASE')
+    userconfig.set('cadc', 'cred_db', 'jcmt')
+    userconfig.set('cadc', 'read_db', 'jcmt')
+    userconfig.set('cadc', 'write_db', 'jcmt')
+
+    # Set the site-dependent databases containing necessary tables
+    if not userconfig.has_section('jcmt'):
+        userconfig.add_section('jcmt')
+    userconfig.set('jcmt', 'caom_db', 'jcmt')
+    userconfig.set('jcmt', 'jcmt_db', 'jcmtmd')
+    userconfig.set('jcmt', 'omp_db', 'jcmtmd')
+
+    userconfigpath = '~/.tools4caom2/jcmt2caom2.config'
+    
+    wrapper = textwrap.TextWrapper(initial_indent='',
+                                   subsequent_indent='')
+    description = '\n\n'.join([
+            wrapper.fill(textwrap.dedent(
+            """
+            Generate healpix files for input to jcmtprocwrap.
+            Output goes to stdout by default, so jcmt_healpix_files can be
+            piped to jcmtprocwrap:
+            """)),
+            'EXAMPLE: jcmt_healpix_files --new | jcmtprocwrap --keeeplog --qsub',
+            wrapper.fill(textwrap.dedent(
+            """
+            The options --daily and --monthly generate healpix files in the 
+            current directory that can also be picked up by jcmtprocwrap,
+            which can be useful if there are a large number of recipe instances.
+            Recipe instances will be grouped by days or months respectively.
+            """)),
+            'EXAMPLE: jcmt_healpix_files --new --daily\n'
+            '         jcmtprocwrap --qsub --keeplog *.healpix',
+            wrapper.fill(textwrap.dedent(
+            """
+            The list of recipe instances to be considered can be restricted 
+            with --fromset=<filepath>
+            where the file contains a list of identity_instance_id values as 
+            decimal integers, one per line as the first token on each line.
+            Lines that do not match this pattern (blank, or a token that is not
+            an integer at the start of the line) will be ignored.
+            """)),
+            wrapper.fill(textwrap.dedent(
+            """
+            The --utdate, --begin and --end arguments can be used to restrict the 
+            range of UT dates to be considered, where recipe instances are dated by 
+            the utdate for the earliest observation in their inputs.  For obs and
+            night recipe instances, this is always the utdate on which all the 
+            data was taken.
+            """)),
+            wrapper.fill(textwrap.dedent(
+            """
+            UT dates can be entered as integers in the format YYYYMMDD, or as small
+            integer offsets from the coming night, i.e. the UT date at 
+            midnight HST tonight.  Thus --utdate=0 is tonight, --utdate=1 is
+            last night, and so forth.  If none of --utdate --begin, --end, or 
+            --fromset is specified, the default is equivalent to --begin=1 --end=0.
+            """)),
+            wrapper.fill(textwrap.dedent(
+            """
+            The --new switch includes only recipe instances that are not already 
+            present in CAOM-2, or have bee reprocessed since their last ingestion.
+            """)),
+            wrapper.fill(textwrap.dedent(
+            """
+            It is possible to combine --fromset with --utdate, --begin, --end, and 
+            --new. 
+            """))])
+
+    ap = argparse.ArgumentParser(
+                description=description,
+                formatter_class=argparse.RawDescriptionHelpFormatter)
+    
+    ap.add_argument('--userconfig',
+                    default=userconfigpath,
+                    help='Optional user configuration file '
+                    '(default=' + userconfigpath + ')')
+    
+    # UTDATE constraints
+    ap.add_argument('--utdate',
+                    type=str,
+                    help='include only a specific utdate')
+    ap.add_argument('--begin',
+                    type=str,
+                    help='include only utdays on or after begin')
+    ap.add_argument('--end',
+                    type=str,
+                    help='include only utdays on or before end')
+
+    # subsets
+    ap.add_argument('--fromset',
+                    type=str,
+                    help='file containing a list of recipe instances')
+    ap.add_argument('--new',
+                    action='store_true',
+                    help='include new or reprocessed recipe instances')
+    
+    # Output options, default = stdout
+    ap.add_argument('--daily',
+                    action='store_true',
+                    help='group by day and output to daily files')
+    ap.add_argument('--monthly',
+                    action='store_true',
+                    help='group by month and output to monthly files')
+
+    # logging
+    ap.add_argument('--log',
+                    default='jcmt_healpix_files_' + utdate_string() + '.log',
+                    help='(optional) name of log file')
+    ap.add_argument('--logdir',
+                    help='(optional) directory to hold log file')
+    ap.add_argument('--debug', '-d',
+                    action='store_true',
+                    help='run ingestion commands in debug mode')
+    a = ap.parse_args()
+    
+    if a.userconfig:
+        userconfigpath = a.userconfig
+    
+    if os.path.isfile(userconfigpath):
+        with open(userconfigpath) as UC:
+            userconfig.readfp(UC)
+
+    caom_db = userconfig.get('jcmt', 'caom_db') + '.dbo.'
+    jcmt_db = userconfig.get('jcmt', 'jcmt_db') + '.dbo.'
+    omp_db =  userconfig.get('jcmt', 'omp_db')  + '.dbo.'
+
+           
+    cwd = os.path.abspath(
+                os.path.expanduser(
+                    os.path.expandvars('.')))
+    
+    if a.logdir:
+        logdir = os.path.abspath(
+                os.path.expanduser(
+                    os.path.expandvars(a.logdir)))
+    else:
+        logdir = cwd
+    
+    loglevel = logging.INFO
+    if a.debug:
+        loglevel = logging.DEBUG
+    
+    if os.path.dirname(a.log):
+        logpath = os.path.abspath(
+                    os.path.expanduser(
+                        os.path.expandvars(a.log)))
+    else:
+        logpath = os.path.join(logdir, a.log)
+
+    log = logger(logpath, loglevel, True)
+    log.file('jcmt2caom2version    = ' + jcmt2caom2version)
+    log.file('tools4caom2version   = ' + tools4caom2version)
+    for attr in dir(a):
+        if attr != 'id' and attr[0] != '_':
+            log.file('%-15s= %s' % (attr, getattr(a, attr)))
+    log.console('logfile = ' + logpath)
+
+    # specifying utdate precludes begin and end
+    if a.utdate and (a.begin or a.end):
+        log.console('specify either utdate or begin/end, not both',
+                    logging.ERROR)
+    
+    if (a.utdate is None and 
+        a.begin is None and
+        a.end is None and
+        a.fromset is None):
+        
+        a.begin = '1'
+        a.end = '0'
+    
+    # if begin is present, but end is not, set end=now
+    if (a.begin is not None and a.end is None):
+        a.end = '0'
+    
+    # if end is present, but bot begin, set begin before start of observatory
+    if (a.end is not None and a.begin is None):
+        a.begin = '19880101'
+            
+    # utdate and begin/end can be absolute or relative to now
+    # Beware of comaprisons between ASCII-coded integers, 
+    # because '2' > '19991231'
+    now = datetime.utcnow()
+    zerotime = datetime(now.year, now.month, now.day, 10, 0, 0)
+    if now.hour >= 10:
+        zerotime += timedelta(1)
+    
+    this_utdate = None
+    if a.utdate is not None:
+        if int(a.utdate) > 19800101:
+            this_utdate = a.utdate
+        else:
+            thisutc = now - timedelta(int(a.utdate))
+            this_utdate = '%04d%02d%02d' % (thisutc.year, 
+                                            thisutc.month, 
+                                            thisutc.day)
+            log.file('%-15s= %s' % ('utdate -> ', this_utdate))
+    
+    this_begin = None
+    if a.begin is not None:
+        if int(a.begin) > 19800101:
+            this_begin = a.begin
+        else:
+            thisutc = now - timedelta(int(a.begin))
+            this_begin = '%04d%02d%02d' % (thisutc.year, 
+                                           thisutc.month, 
+                                           thisutc.day)
+            log.file('%-15s= %s' % ('begin -> ', this_begin))
+
+    this_end = None
+    if a.end is not None:
+        if int(a.end) > 19800101:
+            this_end = a.end
+        else:
+            thisutc = now - timedelta(int(a.end))
+            this_end = '%04d%02d%02d' % (thisutc.year, 
+                                           thisutc.month, 
+                                           thisutc.day)
+            log.file('%-15s= %s' % ('end -> ', this_end))
+    
+    if this_begin and this_end:
+        if this_begin > this_end:
+            store = this_begin
+            this_begin = this_end
+            this_end = store
+    
+    fromset = set()
+    if a.fromset:
+        with open(a.fromset) as RCF:
+            for line in RCF:
+                m = re.match(r'^\s*((0[xX])?[0-9a-f]+)([^\d].*)?$', line)
+                if m:
+                    thisid = str(eval(m.group(1)))
+                    log.file('from includes: "' + thisid + '"',
+                                logging.DEBUG)
+                    fromset.add(thisid)
+    
+    retvals = None
+    with connection(userconfig, log) as db:
+        healpixcmd = '\n'.join([
+            'SELECT rcinst,',
+            '       state,',
+            '       outcount,',
+            '       date_processed,',
+            '       lastModified,',
+            '       substring(uri, patindex("%[12]%", uri), 8) AS utdate',
+            'FROM (',
+            '    SELECT convert(char(20), s.identity_instance_id) AS rcinst,',
+            '           s.state,',
+            '           s.outcount,',
+            '           s.date_processed,',
+            '           MIN(dfi.dp_input) as uri,',
+            '           MAX(ISNULL(u.lastModified, "1980-01-01")) as lastModified',
+            '    FROM (',
+            '        SELECT dri.identity_instance_id,',
+            '               dri.state, ',
+            '               MAX(ISNULL(dri.date_processed,',
+            '                          "1979-12-31 00:00:00")) as date_processed,',
+            '               COUNT(dro.dp_output) AS outcount',
+            '        FROM data_proc.dbo.dp_recipe_instance dri',
+            '           INNER JOIN data_proc.dbo.dp_recipe_output dro on ',
+            '               dri.identity_instance_id = dro.identity_instance_id',
+            '        WHERE  dri.parameters like "-mode=' + "'%'" + 
+            ' -drparameters=' + "'REDUCE_SCAN_JSA_PUBLIC'" + '"',
+            '        GROUP BY dri.identity_instance_id, dri.state',
+            '         ) s',
+            '        INNER JOIN data_proc.dbo.dp_file_input dfi',
+            '            ON s.identity_instance_id=dfi.identity_instance_id',
+            '        LEFT JOIN (',
+            '            SELECT CASE WHEN charindex("x", provenance_runID) = 2 ',
+            '                        THEN hextobigint(provenance_runID) ',
+            '                        ELSE convert(bigint, provenance_runID) ',
+            '                   END as identity_instance_id,',
+            '                   lastModified',
+            '            FROM ' + caom_db + 'caom2_Plane',
+            '            WHERE productID like "healpix%") u',
+            '                ON s.identity_instance_id=u.identity_instance_id',            
+            '    GROUP BY s.identity_instance_id,',
+            '             s.state,',
+            '             s.outcount,',
+            '             s.date_processed) t'])
+        retvals = db.read(healpixcmd)
+        
+        rcinst_dict = {}
+        if retvals:
+            for (identity_instance_id, 
+                 state, 
+                 countout, 
+                 date_processed, 
+                 lastModified, 
+                 utdate) in retvals:
+                 
+                rcinst = str(identity_instance_id).strip()
+
+                if a.fromset and rcinst not in fromset:
+                    continue
+                
+                if ((not a.new or (a.new and lastModified < date_processed)) and
+                    utdate and 
+                    (not this_utdate or (this_utdate == utdate)) and
+                    (not this_begin or ((this_begin <= utdate) and
+                                        (utdate <= this_end)))):
+
+                    if state != 'Y':
+                        log.console('RCINST = ' + rcinst + ' has state=' + 
+                                    state + ' and cannot be ingested',
+                                    logging.WARN)
+                        continue
+                
+                    if not countout:
+                        log.console('RCINST = ' + rcinst + ' produced no '
+                                    'output and cannot be ingested',
+                                    logging.WARN)
+                        continue
+                    
+                    if utdate:
+                        if not re.match(r'^\d{8}$', utdate):
+                            log.console('RCINST = ' + rcinst +' has UTDATE ' + 
+                                        utdate + ' which is not an 8-digit '
+                                        'integer',
+                                        logging.WARN)
+                            continue
+                        
+                        utdatestr = str(utdate)
+                        if a.monthly:
+                            utdatestr = utdatestr[0:6]
+                        elif not a.daily:
+                            utdatestr = 'all'
+                        
+                        if utdatestr not in rcinst_dict:
+                            rcinst_dict[utdatestr] = []
+                        rcinst_dict[utdatestr].append(rcinst)
+                        log.file('PROGRESS: add ' + rcinst + ' on ' + 
+                                    utdatestr)
+            
+            for utdate in rcinst_dict:
+                if utdate == 'all':
+                    for rcinst in sorted(rcinst_dict[utdate]):
+                        print rcinst
+                else:
+                    with open('ut' + utdate + '.healpix', 'w') as RC:
+                        for rcinst in sorted(rcinst_dict[utdate]):
+                            print >>RC, rcinst
